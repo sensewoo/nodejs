9ddd23ae28d2ce12fc8fb1eedcd89eee3011cf15
sensewoo <micael53@gmail.com>
2017-04-04 18:20:22 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var express = require('express');
-	var app = express();
-	
-	app.get('/', function (req, res) {
-		res.send('Hello World!');
-	});
-	
-	app.listen(80, function () {
-  		console.log('Example app listening on port 80!');
-	})
+var express = require('express'),
+    app     = express(),
+    
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
